bb11595a6226b0fe649567c0a375efa52815dd97
Seiji Takahashi <s_takahashi@dreamarts.co.jp>
2016-09-30 13:12:09 +0900

    first commit

---
+++ resources/sql/queries.sql
@@ -0,0 +1,16 @@
+-- :name save-message! :! :n
+-- :doc creates a new message
+INSERT INTO guestbook(
+  name
+ ,message
+ ,timestamp
+) VALUES (
+  :name
+ ,:message
+ ,:timestamp  
+)
+
+-- :name get-messages :? :*
+-- :doc selects all available messages
+SELECT *
+  FROM gustbook
